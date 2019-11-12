cabe16ac8e00ed1e1eab6446c7023accdc475899
Thomas Winterling <info@winterling-labs.com>
2019-11-12 14:30:51 +0100

    Update

---
+++ helper.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @package    WL LIVEDATA MODUL
+ *
+ * @author     Thomas Winterling <info@winterling-labs.com>
+ * @copyright  Copyright (C) 2005 - 2019. All rights reserved.
+ * @license    http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
+ **/
+
+defined('_JEXEC') or die;
+
+/**
+ * Helper for mod_livedata_module
+ *
+ * @since  Version 1.0.0.0
+ */
+class ModWL_LIVEDATA_Module_Helper
+{   
+    
+    
+
+}
---
+++ tmpl/default.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package    WL_LIVEDATA_MODULE
+ *
+ * @author     Thomas Winterling <info@winterling-labs.com>
+ * @copyright  Copyright (C) 2011 - 2019
+ * @license    http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
+ **/
+
+defined('_JEXEC') or die;
+
+JHtml::_('stylesheet', 'mod_wl_livedata_module/style.css', array('version' => 'auto', 'relative' => true));
+JHtml::_('script', 'mod_wl_livedata_module/scripts.js', array('version' => 'auto', 'relative' => true));
+JHtml::_('jQuery.Framework');
+?>
